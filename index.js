b2ff20bba69f6ace5fb6f2bc80ee52122152029d
Fabricegashati <gashati.tech@gmail.com>
2024-10-20 02:47:31 +0200

    modify the origin url

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: ['https://isoko-dqe.pages.dev/'],
+		origin: ['https://isoko-dqe.pages.dev'],
 		credentials: true,
 		methods: ['GET', 'POST', 'PUT', 'DELETE'],
 	})
