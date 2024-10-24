e697df1de915ffd900c24ff991a100b8e54dffab
Fabricegashati <gashati.tech@gmail.com>
2024-10-24 09:29:28 +0200

    changes

--- controllers/provider.js
+++ controllers/provider.js
@@ -167,7 +167,7 @@ const loginProvider = async (req, res) => {
 
 		res.cookie('refreshToken', refreshToken, {
 			httpOnly: true,
-			secure: process.env.NODE_ENV === 'production',
+			secure: true,
 			sameSite: 'Strict', // Prevent CSRF attacks
 			maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 		});
